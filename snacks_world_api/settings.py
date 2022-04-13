8acbf2a41199327fe8ef7a25a5ff0b253d56129e
kathleendiep <kathleendiep@gmail.com>
2022-04-13 11:17:28 -0700

    env

--- snacks_world_api/settings.py
+++ snacks_world_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import dj_database_url # add this
 import os # add this
-# from dotenv import load_dotenv, find_dotenv
+import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # edit this var
 # this is generic one: 
@@ -30,9 +30,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # UPDATE secret key
-load_dotenv(find_dotenv())
-SECRET_KEY = os.environ['SECRET_KEY']
 
+# Add .env variables anywhere before SECRET_KEY
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+# UPDATE secret key
+SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 DEBUG = True
 ALLOWED_HOSTS = ['localhost', 'snacksworld-api.herokuapp.com'] # 
