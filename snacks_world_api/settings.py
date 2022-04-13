000ee7bc3f6113adde08b1aed7852dbb42d3f462
kathleendiep <kathleendiep@gmail.com>
2022-04-13 11:23:15 -0700

    fixed

--- snacks_world_api/settings.py
+++ snacks_world_api/settings.py
@@ -20,7 +20,6 @@ import dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 # in env
 # SECURITY WARNING: don't run with debug turned on in production!
