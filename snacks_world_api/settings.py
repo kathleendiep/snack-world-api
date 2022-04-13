fba8afb304a0cefda6e0ea905eb678935bc25f6b
kathleendiep <kathleendiep@gmail.com>
2022-04-13 15:42:36 -0700

    fixed dotenv

--- snacks_world_api/settings.py
+++ snacks_world_api/settings.py
@@ -13,7 +13,7 @@ from pathlib import Path
 import dj_database_url # add this
 import os # add this
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # edit this var
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # edit this var
 # this is generic one:
 
 # Quick-start development settings - unsuitable for production
