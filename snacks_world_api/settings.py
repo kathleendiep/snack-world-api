7f2a448d3c6fb463d815ec2aba29cd7f02fe331f
kathleendiep <kathleendiep@gmail.com>
2022-04-13 14:56:55 -0700

    Added string

--- snacks_world_api/settings.py
+++ snacks_world_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # UPDATE secret key
 # SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
-SECRET_KEY = os.environ.get["SECRET_KEY"]
+SECRET_KEY = str(os.environ.get("SECRET_KEY"))
 DEBUG = True
 ALLOWED_HOSTS = ['localhost', 'snacksworld-api.herokuapp.com'] # 
 
