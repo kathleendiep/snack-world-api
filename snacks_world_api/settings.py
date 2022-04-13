57051a1c98a1617b2c5fcedd71123b74ae80ccc5
kathleendiep <kathleendiep@gmail.com>
2022-04-13 13:53:27 -0700

    added

--- snacks_world_api/settings.py
+++ snacks_world_api/settings.py
@@ -16,18 +16,14 @@ import os # add this
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # edit this var
 # this is generic one: 
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # edit this var
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 # UPDATE secret key
-SECRET_KEY = str(os.environ['SECRET_KEY']) # Instead of your actual secret key
+SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 DEBUG = True
 ALLOWED_HOSTS = ['localhost', 'snacksworld-api.herokuapp.com'] # 
