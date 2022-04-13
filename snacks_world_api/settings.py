9fa4190f2859d9fc411e186b4da7ce6ff2f05ad2
kathleendiep <kathleendiep@gmail.com>
2022-04-13 15:41:36 -0700

    fixed dotenv

--- snacks_world_api/settings.py
+++ snacks_world_api/settings.py
@@ -14,9 +14,7 @@ import dj_database_url # add this
 import os # add this
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # edit this var
-# this is generic one: 
-from dotenv import load_dotenv
-load_dotenv()
+# this is generic one:
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
