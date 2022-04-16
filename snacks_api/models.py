0266b07e7626a4e2ab11915f8d044edfc96a1f33
kathleendiep <kathleendiep@gmail.com>
2022-04-16 10:38:30 -0700

    added pic upload

--- snacks_api/models.py
+++ snacks_api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+def nameFile(instance, filename):
+    return '/'.join(['images', str(instance.name), filename])
 
 # Create your models here.
 class Snack(models.Model):
@@ -7,4 +9,11 @@ class Snack(models.Model):
     city = models.CharField(max_length=32)
     country = models.CharField(max_length=32)
     description = models.CharField(max_length=300)
-    image = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='uploaded_image', blank=True, null=True)
+
+
+
+
+
+
+    
--- snacks_api/views.py
+++ snacks_api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .serializers import SnackSerializer
 from .models import Snack
+# to define function 
+from django.http import JsonResponse
+from django.http import HttpResponseRedirect, HttpResponse
+import json
 
 # Create your views here.
 # controller function! (view in django - handles controls)
@@ -12,3 +16,9 @@ class SnackList(generics.ListCreateAPIView):
 class SnackDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snack.objects.all().order_by('id')
     serializer_class = SnackSerializer
+
+#  define functions
+def post(self, request, *args, **kwargs):
+    file = request.data['file']
+    image = Snack.objects.create(image=file)
+    return HttpResponse(json.dumps({'message': "Uploaded"}), status=200)
